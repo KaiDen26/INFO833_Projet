35279f4eaa9b009732a78481a3ce50ec9e544264
Thomas Nicolas <thomas26.nicolas@gmail.com>
2023-03-26 08:24:31 +0200

    Restructuration

--- src/fr/usmb/dht/Initializer.java
+++ src/fr/usmb/dht/Initializer.java
@@ -4,50 +4,57 @@ import peersim.edsim.*;
 import peersim.core.*;
 import peersim.config.*;
 
-/*
-  Module d'initialisation de helloWorld: 
-  Fonctionnement:
-    pour chaque noeud, le module fait le lien entre la couche transport et la couche applicative
-    ensuite, il fait envoyer au noeud 0 un message "Hello" a tous les autres noeuds
- */
+	/*
+	  Module d'initialisation de helloWorld: 
+	  Fonctionnement:
+	    pour chaque noeud, le module fait le lien entre la couche transport et la couche applicative
+	    ensuite, il fait envoyer au noeud 0 un message "Hello" a tous les autres noeuds
+	 */
+
 public class Initializer implements peersim.core.Control {
     
-    private int helloWorldPid;
+    private int dhtPid;
 
     public Initializer(String prefix) {
-	//recuperation du pid de la couche applicative
-	this.helloWorldPid = Configuration.getPid(prefix + ".helloWorldProtocolPid");
+    	
+    	// recuperation du pid de la couche applicative
+    	this.dhtPid = Configuration.getPid(prefix + ".dhtProtocolPid");
+	
     }
 
     public boolean execute() {
-	int nodeNb;
-	DhtNode emitter, current;
-	Node dest;
-	Message helloMsg;
-
-	//recuperation de la taille du reseau
-	nodeNb = Network.size();
-	//creation du message
-	helloMsg = new Message(Message.HELLOWORLD,"Hello!!");
-	if (nodeNb < 1) {
-	    System.err.println("Network size is not positive");
-	    System.exit(1);
-	}
-
-	//recuperation de la couche applicative de l'emetteur (le noeud 0)
-	emitter = (DhtNode)Network.get(0).getProtocol(this.helloWorldPid);
-	emitter.setTransportLayer(0);
-
-	//pour chaque noeud, on fait le lien entre la couche applicative et la couche transport
-	//puis on fait envoyer au noeud 0 un message "Hello"
-	for (int i = 1; i < nodeNb; i++) {
-	    dest = Network.get(i);
-	    current = (DhtNode)dest.getProtocol(this.helloWorldPid);
-	    current.setTransportLayer(i);
-	    emitter.send(helloMsg, dest);
-	}
-
-	System.out.println("Initialization completed");
-	return false;
+    	
+    	// recuperation de la taille du reseau
+		int nodeNb = Network.size();
+		
+		DhtNode current;
+		Node destination;
+		
+		// creation du message
+		Message helloMsg = new Message(Message.HELLOWORLD,"Hello!!");
+		
+		
+		if (nodeNb < 1) {
+		    System.err.println("Network size is not positive");
+		    System.exit(1);
+		}
+	
+		// recuperation de la couche applicative de l'emetteur (le noeud 0)
+		DhtNode emitter = (DhtNode)Network.get(0).getProtocol(this.dhtPid);
+		emitter.setTransportLayer(0);
+	
+		// pour chaque noeud, on fait le lien entre la couche applicative et la couche transport
+		// puis on fait envoyer au noeud 0 un message "Hello"
+		
+		for (int i = 1; i < nodeNb; i++) {
+		    destination = Network.get(i);
+		    current = (DhtNode) destination.getProtocol(this.dhtPid);
+		    current.setTransportLayer(i);
+		    emitter.send(helloMsg, destination);
+		}
+	
+		System.out.println("Initialization completed");
+		return false;
     }
+    
 }
--- src/fr/usmb/dht/Message.java
+++ src/fr/usmb/dht/Message.java
@@ -1,7 +1,5 @@
 package fr.usmb.dht;
 
-import peersim.edsim.*;
-
 public class Message {
 
     public final static int HELLOWORLD = 0;
@@ -10,16 +8,16 @@ public class Message {
     private String content;
 
     Message(int type, String content) {
-	this.type = type;
-	this.content = content;
+    	this.type = type;
+		this.content = content;
     }
 
     public String getContent() {
-	return this.content;
+    	return this.content;
     }
 
     public int getType() {
-	return this.type;
+    	return this.type;
     }
     
 }
